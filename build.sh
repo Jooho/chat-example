844239b16d5e03a5bef940d55435cf394f3f8570
root <root@master1.milton.internal>
2018-10-19 02:26:23 +0000

    build.sh fix

--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
 
 
 export tag=$1
-export old_version
 
 if [[ $tag == v2 ]]
 then
-  old_version = v1
+  export old_version=v1
 else
-  old_version = v2
+  export old_version=v2
 fi
 
-sed -i "s/$old_version/$tag/g" server.js
+sed -i "s/${old_version}/$tag/g" server.js
 docker pull $(oc get is  chat-server-example -o jsonpath='{.status.dockerImageRepository}')
 docker tag $(oc get is  chat-server-example -o jsonpath='{.status.dockerImageRepository}') docker.io/ljhiyh/chat-server:$tag
 docker push docker.io/ljhiyh/chat-server:$tag
