a653ea44ceeb56bdba2d0daf84304022a90bef9d
root <root@master1.milton.internal>
2018-10-19 14:30:41 +0000

    update version

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 
+if [[ z$1 == z ]]
+then
+  echo "Please specify version name : v1 or v2"
+  echo "ex) ./build.sh v1"
+  exit 1
+fi
 
 export tag=$1
 
@@ -10,6 +16,11 @@ else
 fi
 
 sed -i "s/${old_version}/$tag/g" server.js
+
+git commit -am "update version"
+git push
+
+oc start-build chat-server-example -w
 docker pull $(oc get is  chat-server-example -o jsonpath='{.status.dockerImageRepository}')
 docker tag $(oc get is  chat-server-example -o jsonpath='{.status.dockerImageRepository}') docker.io/ljhiyh/chat-server:$tag
 docker push docker.io/ljhiyh/chat-server:$tag
